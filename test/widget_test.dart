0207d76d4d190104101cab44eab2d6ef05d512a2
Allan Nava <allannava95@gmail.com>
2020-09-16 21:32:58 +0200

    need to fix the widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:corona_italy/main.dart';
 
+import '../lib/app/app.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CoronaItaly());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
